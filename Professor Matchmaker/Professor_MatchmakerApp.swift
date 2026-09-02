44a27695f11113d20655fabbef824c67acee40dd
Bardia Seiri <bardiaseiri@gmail.com>
2026-09-01 22:57:32 -0700

    Initial Commit

---
+++ Professor Matchmaker/Professor_MatchmakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Professor_MatchmakerApp.swift
+//  Professor Matchmaker
+//
+//  Created by Bardia Seiri on 9/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Professor_MatchmakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
